71ef3831413f4266c841530fcd9d6a464f8ad5d2
dolodorsey <dolodorsey@gmail.com>
2026-08-03 17:21:12 -0400

    wire ON CALL premium experience shell

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import "./native.js";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './premium-experience.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div className="oc-experience" data-app="on-call">
+      <App />
+    </div>
   </React.StrictMode>,
 )
